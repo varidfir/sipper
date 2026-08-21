32e45847fe43817fd56f070ac5c504e5c9df7382
varidfir <varidfir26@gmail.com>
2026-08-21 10:55:47 +0700

    perbaikan penulisan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 import './bootstrap';
+
+const uppercaseFieldSelector = [
+	'.form-content input[type="text"]',
+	'.form-content textarea',
+	'.wilayah-form-content input[type="text"]',
+].join(', ');
+
+const uppercaseField = (field) => {
+	field.value = field.value.toUpperCase();
+};
+
+document.addEventListener('input', (event) => {
+	if (event.target.matches(uppercaseFieldSelector)) {
+		uppercaseField(event.target);
+	}
+});
+
+document.addEventListener('submit', (event) => {
+	event.target.querySelectorAll(uppercaseFieldSelector).forEach(uppercaseField);
+});
